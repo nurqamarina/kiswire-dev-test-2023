8c1dea9f1720470a242d76d8ec0fbba8b6bcbe95
Nur Qamarina Tuah <nqrinatuah@gmail.com>
2023-05-30 21:06:36 +0800

    Done

--- Question 3/Program.cs
+++ Question 3/Program.cs
@@ -2,7 +2,7 @@
 Question 3
 Write a C# program that takes a string as input and checks whether it is a palindrome or not. A
 palindrome is a word, phrase, number, or other sequence of characters that reads the same forward
-and backward, ignoring spaces, punctuation, and capitalization.
+and backward, ignoring spaces, punctuation, and capitalization..
 */
 
 using System;
@@ -24,7 +24,3 @@ class Program{
         return true;
     }
 }
-
-
-
-
