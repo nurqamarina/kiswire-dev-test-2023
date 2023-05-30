ca2f4cd6545e7e46548d6a24f89740e51f1fe13f
Nur Qamarina Tuah <nqrinatuah@gmail.com>
2023-05-30 19:30:05 +0800

    question 1 done

---
+++ Question 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program{
+private static int SumToN(int n)
+{
+    int result = 0;
+
+    for (int i =1; i <= n;++i)
+    {
+        result += i;
+    }
+    return result;
+}
+public static void Main(){
+    Console.WriteLine("Sum of integers up to N={0} is: {1}",5,SumToN(5));
+}
+}
